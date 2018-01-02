cccdd9be1791dec34cd35d35b327e509d6c2ef6d
הראל כהן <harelchn@gmail.com>
2018-01-02 23:57:46 +0200

    יצחק ילד חמוווווווד

---
+++ dotNet5778_project01_315285494_211486493/dotNet5778_project01_315285494_211486493/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5778_project01_315285494_211486493
+{
+    public class Class1
+    {
+    }
+}
